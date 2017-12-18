4f39c05c80db5feda5c96e297a49337cccab5429
torrence <torrence@mail.ustc.edu.cn>
2017-12-18 11:08:44 +0800

    FORMAT #include files

--- MonitorBuilder.C
+++ MonitorBuilder.C
@@ -1,123 +1,45 @@
 // Mainframe macro generated from application: /mnt/j/cpp/root/root_v6_08_06/bin/root.exe
 // By ROOT version 6.08/06 on 2017-12-16 00:27:13
 
-#ifndef ROOT_TGDockableFrame
 #include "TGDockableFrame.h"
-#endif
-#ifndef ROOT_TGMenu
 #include "TGMenu.h"
-#endif
-#ifndef ROOT_TGMdiDecorFrame
 #include "TGMdiDecorFrame.h"
-#endif
-#ifndef ROOT_TG3DLine
 #include "TG3DLine.h"
-#endif
-#ifndef ROOT_TGMdiFrame
 #include "TGMdiFrame.h"
-#endif
-#ifndef ROOT_TGMdiMainFrame
 #include "TGMdiMainFrame.h"
-#endif
-#ifndef ROOT_TGMdiMenu
 #include "TGMdiMenu.h"
-#endif
-#ifndef ROOT_TGColorDialog
 #include "TGColorDialog.h"
-#endif
-#ifndef ROOT_TGColorSelect
 #include "TGColorSelect.h"
-#endif
-#ifndef ROOT_TGListBox
 #include "TGListBox.h"
-#endif
-#ifndef ROOT_TGNumberEntry
 #include "TGNumberEntry.h"
-#endif
-#ifndef ROOT_TGScrollBar
 #include "TGScrollBar.h"
-#endif
-#ifndef ROOT_TGComboBox
 #include "TGComboBox.h"
-#endif
-#ifndef ROOT_TGuiBldHintsEditor
 #include "TGuiBldHintsEditor.h"
-#endif
-#ifndef ROOT_TGuiBldNameFrame
 #include "TGuiBldNameFrame.h"
-#endif
-#ifndef ROOT_TGFrame
 #include "TGFrame.h"
-#endif
-#ifndef ROOT_TGFileDialog
 #include "TGFileDialog.h"
-#endif
-#ifndef ROOT_TGShutter
 #include "TGShutter.h"
-#endif
-#ifndef ROOT_TGButtonGroup
 #include "TGButtonGroup.h"
-#endif
-#ifndef ROOT_TGCanvas
 #include "TGCanvas.h"
-#endif
-#ifndef ROOT_TGFSContainer
 #include "TGFSContainer.h"
-#endif
-#ifndef ROOT_TGFontDialog
 #include "TGFontDialog.h"
-#endif
-#ifndef ROOT_TGuiBldEditor
 #include "TGuiBldEditor.h"
-#endif
-#ifndef ROOT_TGButton
 #include "TGButton.h"
-#endif
-#ifndef ROOT_TGFSComboBox
 #include "TGFSComboBox.h"
-#endif
-#ifndef ROOT_TGLabel
 #include "TGLabel.h"
-#endif
-#ifndef ROOT_TGMsgBox
 #include "TGMsgBox.h"
-#endif
-#ifndef ROOT_TRootGuiBuilder
 #include "TRootGuiBuilder.h"
-#endif
-#ifndef ROOT_TGTab
 #include "TGTab.h"
-#endif
-#ifndef ROOT_TGListView
 #include "TGListView.h"
-#endif
-#ifndef ROOT_TGSplitter
 #include "TGSplitter.h"
-#endif
-#ifndef ROOT_TGStatusBar
 #include "TGStatusBar.h"
-#endif
-#ifndef ROOT_TGListTree
 #include "TGListTree.h"
-#endif
-#ifndef ROOT_TGuiBldGeometryFrame
 #include "TGuiBldGeometryFrame.h"
-#endif
-#ifndef ROOT_TGToolTip
 #include "TGToolTip.h"
-#endif
-#ifndef ROOT_TGToolBar
 #include "TGToolBar.h"
-#endif
-#ifndef ROOT_TRootEmbeddedCanvas
 #include "TRootEmbeddedCanvas.h"
-#endif
-#ifndef ROOT_TCanvas
 #include "TCanvas.h"
-#endif
-#ifndef ROOT_TGuiBldDragManager
 #include "TGuiBldDragManager.h"
-#endif
 
 #include "Riostream.h"
 
